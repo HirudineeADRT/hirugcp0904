16e8f618e1b6021a05566d136c5853269dfe91b9
HirudineeADRT <hirudinee@adroitlogic.com>
2020-04-09 10:58:07 +0530

    deploy GCP

---
+++ main.py
@@ -0,0 +1,5 @@
+# This file is used to register all your cloud functions in GCP.
+# DO NOT EDIT/DELETE THIS, UNLESS YOU KNOW WHAT YOU ARE DOING.
+
+from hirugcp0904 import handler as hirugcp0904
+from testgap123 import handler as testgap123
